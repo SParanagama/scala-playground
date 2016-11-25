4c3d799e4148100284e2dbbbd2fe365369d731a9
SParanagama <sagara89dev@gmail.com>
2016-11-25 11:16:57 +0530

    Adding map and flatmap questions

---
+++ src/main/scala/FlatmapExamples.scala
@@ -0,0 +1,28 @@
+/**
+ * Class for
+ */
+
+/**
+ *
+ * Think of Flatmap as a 'Map' and a 'Flatten' operation. The Map operation should give a traversable object.
+ * Then, scala will take care of flattening each traversable object to make one traversable object.
+ * (Its like combining a list of lists to make one large list)
+ *
+ */
+
+object FlatmapExamples {
+
+  def run() = {
+
+    val charListOfList = List( List("a", "b"), List("c", "d", "e"), List("f", "g", "h") )
+
+    // Q1. Make a new list having each character of the above 'charListOfList' and convert each character to upper case
+
+
+    // Q2. Read 'data.txt' and count the number of words in it.
+    // Hint: To count the number of words, you need one list having each word.
+
+
+
+  }
+}
---
+++ src/main/scala/MapExamples.scala
@@ -0,0 +1,32 @@
+/**
+ * Class for
+ */
+object MapExamples {
+
+  def run() = {
+
+    // Define a list of integers
+    val integerList = List(1, 2, 3, 4, 5)
+
+    // Q1. Make a new list, adding 1 to each element of 'integerList'
+
+
+    // Q2. Make a new list, multiplying each element of 'integerList' by 5
+
+
+    // Q3. Make a new list, converting integer to its string type
+
+
+    val nameList = List("adam, john", "tom", "betty")
+
+
+    // Q4. Make a new list where each name in 'nameList' is in upper case
+
+
+    val nameIds = Map( "adam" -> 4, "john" -> 21, "tom" -> 2, "betty" -> 1, "sam" -> 9, "harry" -> 20 )
+
+    // Q5. Using the above 'nameIds', make a new list having the ids of the corresponding name
+
+
+  }
+}
