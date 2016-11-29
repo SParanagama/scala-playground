fa3bd16b2855aa05beafb61c034124796dc68fa4
SParanagama <sagara89dev@gmail.com>
2016-11-29 15:45:50 +0530

    Added new problems

---
+++ src/main/scala/ImportantScalaStuff.scala
@@ -0,0 +1,54 @@
+/**
+ * Class for
+ */
+object ImportantScalaStuff {
+  
+  def run = {
+    
+    // Q1. Define a list of integers having values 1,3,6,8,10
+    
+    
+    // Q2. Print the above list
+    
+    
+    // Q3. Define a map having the following key value pairs
+    // "apples" ---> 3 , "oranges" ---> 25, "pears" ---> 12
+    
+    
+    // Q4. What is the type of the above map?
+    
+    
+    // Q5. What is the difference between mutable and immutable collections in scala? (Don't google the exact question :P )
+    
+    
+    // Q6. Is the map you defined in Q3 mutable or immutable?
+    
+    
+    // Q7. Define a mutable empty list of type String
+    
+    
+    // Q8. Add the following values to it
+    // "Adam" , "Peter" , "Betty"
+
+
+    // Q9. Define a mutable empty map of type [String, Boolean]
+
+
+    // Q10. Add the following key value pairs to it:
+    // "apples" ---> 3 , "oranges" ---> 25, "pears" ---> 12
+
+
+    // Q11. Print each key value pair in the map
+
+
+    // Q12. Add another key value pair "grapes" ---> 4
+
+
+    // Q13. Print the above map
+    
+
+    
+    
+  }
+
+}
